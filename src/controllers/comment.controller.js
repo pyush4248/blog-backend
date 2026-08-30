144f35cc49d87f978d38d1a6a1acba6053ba72ff
Piyush Kumar Jaiswal <112476313+pyush4248@users.noreply.github.com>
2026-08-30 11:54:20 +0530

    Remove duplicate mongoose import
    Removed duplicate import of mongoose in comment.controller.js

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -6,8 +6,6 @@ import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import mongoose from "mongoose";
 
-import mongoose from "mongoose";
-
 const createComment = asyncHandler(async (req, res) => {
 
     const {
